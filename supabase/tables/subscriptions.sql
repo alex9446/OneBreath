08d724ebd6307024b2551f35f191417bb1180467
Alessio Margelli <alex9446@gmail.com>
2025-11-08 00:08:27 +0100

    added missing table and view

---
+++ supabase/tables/subscriptions.sql
@@ -0,0 +1,9 @@
+create table public.subscriptions (
+  id uuid not null default gen_random_uuid (),
+  created_at timestamp with time zone not null default now(),
+  user_id uuid not null,
+  subscription_json jsonb not null,
+  last_status_code smallint null,
+  constraint subscriptions_pkey primary key (id),
+  constraint subscriptions_user_id_fkey foreign KEY (user_id) references auth.users (id) on delete CASCADE
+) TABLESPACE pg_default;
---
+++ supabase/views/myattendances.sql
@@ -0,0 +1,12 @@
+create view public.myattendances
+with
+  (security_invoker = true) as
+select
+  a.user_id,
+  to_char(a.marked_day, 'DD/MM/YY') as marked_day,
+  g.name as group_name
+from
+  public.attendances as a
+  join public.groups as g on a.group_id = g.id
+order by
+  a.marked_day desc;
