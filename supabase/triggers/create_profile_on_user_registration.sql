c336edcbcd4e9911fad955ae1edc17b7c9dcfe65
Alessio Margelli <alex9446@gmail.com>
2025-11-16 15:12:26 +0100

    transferred function from public to private schema

--- supabase/triggers/create_profile_on_user_registration.sql
+++ supabase/triggers/create_profile_on_user_registration.sql
@@ -1,5 +1,5 @@
 -- inserts a row into public.profiles
-create function public.handle_new_user()
+create function private.handle_new_user()
 returns trigger
 language plpgsql
 security definer set search_path = ''
@@ -14,4 +14,4 @@ $$;
 -- trigger the function every time a user is created
 create trigger on_auth_user_created
   after insert on auth.users
-  for each row execute procedure public.handle_new_user();
+  for each row execute procedure private.handle_new_user();
