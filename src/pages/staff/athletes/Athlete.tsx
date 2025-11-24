f5838d7773d1c561055b9c7a78c177858f3f69bb
Alessio Margelli <alex9446@gmail.com>
2025-11-24 13:28:18 +0100

    added confirmation dialog for insert admin

--- src/pages/staff/athletes/Athlete.tsx
+++ src/pages/staff/athletes/Athlete.tsx
@@ -7,6 +7,13 @@ import ErrorBox from '../../../components/ErrorBox'
 import UserAttendances from '../../../components/UserAttendances'
 import './Athlete.sass'
 
+const confirmMessage = (name: string) => `
+Confermi di voler elevare ${name} a staff?
+L'azione non è annullabile!
+
+Affinche la modifica sia visibile, ${name} dovrà effettuare nuovamente il login
+`.trim()
+
 type AthleteProps = {
   profile: Pick<Tables<'profiles'>, 'id' | 'first_name' | 'last_name' | 'group_id'>
   admin: boolean
@@ -17,12 +24,14 @@ const Athlete: Component<AthleteProps> = (props) => {
   const supabaseClient = useSupabase()
 
   const insertAdmin = action(async () => {
+    if (!window.confirm(confirmMessage(props.profile.first_name))) return false
     const currentAdminId = await getUserId(supabaseClient)
     const { error } = await supabaseClient.from('admins').insert([
       { id: props.profile.id, added_by: currentAdminId }
     ])
     if (error) throw error.message
     await props.adminsRefetch()
+    return true
   })
   const useInsertAdmin = useAction(insertAdmin)
   const submission = useSubmission(insertAdmin)
@@ -36,7 +45,7 @@ const Athlete: Component<AthleteProps> = (props) => {
       <p>Presenze di {props.profile.first_name} {props.profile.last_name}</p>
       <button class='add-admin' disabled={props.admin || submission.pending}
               onClick={insertAdminClick}>
-        {props.admin ? 'Già in staff' : 'Aggiungi a staff'}
+        {props.admin ? (submission.result ? 'Fatto!' : 'Già in staff') : 'Aggiungi a staff'}
       </button>
       <ErrorBox>{submission.error}</ErrorBox>
       <UserAttendances id={props.profile.id} />
