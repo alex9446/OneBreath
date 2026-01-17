0e5d873897824d20d2fc054fcc389d6d884be18c
Alessio Margelli <alex9446@gmail.com>
2026-01-17 23:37:47 +0100

    inverted sectrions of the dynamic title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,12 +1,12 @@
 import { onCleanup, onMount, type ParentComponent } from 'solid-js'
 
-const baseTitle = 'Soci OneBreath - '
+const baseTitle = ' - Soci OneBreath'
 const defaultTitle = 'Soci OneBreath'
 
 const Title: ParentComponent = (props) => {
   onMount(() => {
     if (typeof props.children === 'string') {
-      document.title = baseTitle + props.children
+      document.title = props.children + baseTitle
     } else {
       document.title = defaultTitle
     }
