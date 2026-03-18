4b9d446b1a21bbf8a0cad0bceb85ab029847bc6b
Alessio Margelli <alex9446@gmail.com>
2026-03-18 19:43:44 +0100

    enable the URL setting in push notifications

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,19 +1,37 @@
 /// <reference lib="webworker" />
 declare const self: ServiceWorkerGlobalScope
 
-// self.addEventListener('install', () => self.skipWaiting())
+type NotificationData = {
+  url: string
+}
+
+self.addEventListener('install', () => self.skipWaiting())
+self.addEventListener('activate', () => self.clients.claim())
 
 self.addEventListener('push', (event) => {
   if (!event.data) return
-  const title = event.data.text()
+  let title = event.data.text()
+  let url = '/'
+  try {
+    const dataJson = event.data.json()
+    title = dataJson.title ?? title
+    url = dataJson.url ?? url
+  } catch (error) { console.warn('PushMessageData is not json', error) }
+
   const options: NotificationOptions = {
-    icon: '/icon.svg'
+    icon: '/icon.svg',
+    data: { url } as NotificationData
   }
 
   event.waitUntil(self.registration.showNotification(title, options))
 })
 
 self.addEventListener('notificationclick', (event) => {
+  let url = '/'
+  try {
+    ({ url } = event.notification.data as NotificationData)
+  } catch (error) { console.error(error) }
+
   event.notification.close()
-  event.waitUntil(self.clients.openWindow('/'))
+  event.waitUntil(self.clients.openWindow(url))
 })
