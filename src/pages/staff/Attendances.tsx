ef8567c0089b8c3c60b0468c24334cc93f6d7b84
Alessio Margelli <alex9446@gmail.com>
2026-05-05 23:53:28 +0200

    makes input value reactive to params changes

--- src/pages/staff/Attendances.tsx
+++ src/pages/staff/Attendances.tsx
@@ -1,4 +1,4 @@
-import { createResource, For } from 'solid-js'
+import { createMemo, createResource, For } from 'solid-js'
 import { A, useNavigate, useParams } from '@solidjs/router'
 import { getGroupFromLS, getTodayDate } from '../../utils/mixed'
 import { useSupabase } from '../../utils/context'
@@ -9,28 +9,23 @@ import Icon from '../../components/Icon'
 import FakeButton from '../../components/FakeButton'
 import './Attendances.sass'
 
-const splitGroupDate = (groupDate: string | undefined, today: string): [number, string] => {
-  if (!groupDate) return [ getGroupFromLS(), today ]
-  const groupDateSplitted = groupDate.split('g')
-  return [ parseInt(groupDateSplitted[0]), groupDateSplitted[1] ]
-}
-
 const Attendances = () => {
   const navigate = useNavigate()
   const supabaseClient = useSupabase()
   const params = useParams()
   const todayDate = getTodayDate()
-  const [defaultGroup, defaultDate] = splitGroupDate(params.groupDate, todayDate)
+  const options = createMemo(() => {
+    if (!params.groupDate) return { group: getGroupFromLS(), date: todayDate }
+    const groupDateSplitted = params.groupDate.split('g')
+    return { group: parseInt(groupDateSplitted[0]), date: groupDateSplitted[1] }
+  })
 
-  const [attendances, {mutate}] = createResource(
-    () => splitGroupDate(params.groupDate, todayDate),
-    async ([group, date]) => {
-      const { data: attendances, error } = await supabaseClient.from('attendances_with_name')
-        .select('user_id,name').eq('group_id', group).eq('marked_day', date)
-      if (error) throw error.message
-      return attendances
-    }
-  )
+  const [attendances, {mutate}] = createResource(options, async ({ group, date }) => {
+    const { data: attendances, error } = await supabaseClient.from('attendances_with_name')
+      .select('user_id,name').eq('group_id', group).eq('marked_day', date)
+    if (error) throw error.message
+    return attendances
+  })
 
   let selectGroup!: HTMLSelectElement
   let inputDate!: HTMLInputElement
@@ -57,12 +52,12 @@ const Attendances = () => {
   return (<>
     <Title>Area staff &gt; Storico presenze</Title>
     <main id='attendances-page'>
-      <SelectGroup ref={selectGroup} defaultOption={defaultGroup} onInput={onInputEvent} />
+      <SelectGroup ref={selectGroup} defaultOption={options().group} onInput={onInputEvent} />
       <div class='date-selector'>
         <Icon path={W12H24MenuLeft} width={18} viewBox='12 24'
               title='giorno precedente' onClick={() => changeDate(-1)} />
         <input type='date' ref={inputDate} required onInput={onInputEvent}
-              value={defaultDate} min='2025-10-23' max={todayDate} />
+               value={options().date} min='2025-10-23' max={todayDate} />
         <Icon path={W12H24MenuRight} width={18} viewBox='12 24'
               title='giorno successivo' onClick={() => changeDate(1)} />
       </div>
