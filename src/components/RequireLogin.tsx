268eebc471f2d52f8ef62689b94f8c53981979bf
Alessio Margelli <alex9446@gmail.com>
2026-05-29 19:04:51 +0200

    wrapped code that wasn't inside onMount

--- src/components/RequireLogin.tsx
+++ src/components/RequireLogin.tsx
@@ -1,4 +1,4 @@
-import { createSignal, Show, type ParentComponent } from 'solid-js'
+import { createSignal, Show, onMount, type ParentComponent } from 'solid-js'
 import { useNavigate } from '@solidjs/router'
 import { useSupabase } from '../utils/context'
 import LoggedOnMount from './LoggedOnMount'
@@ -8,9 +8,9 @@ const RequireLogin: ParentComponent = (props) => {
   const navigate = useNavigate()
   const supabaseClient = useSupabase()
 
-  supabaseClient.auth.getSession().then(({ data }) => {
+  onMount(() => supabaseClient.auth.getSession().then(({ data }) => {
     data.session === null ? navigate('/login') : setLoggedIn(true)
-  })
+  }))
 
   return (
     <Show when={loggedIn()} fallback={<main><p>Verifica login...</p></main>}>
