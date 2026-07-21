714e3b8fb070367ff15265cf37f79b4afddb1833
Alessio Margelli <alex9446@gmail.com>
2026-07-21 23:52:15 +0200

    direct use of ref to show modal

--- src/components/DomainChangeAlert.tsx
+++ src/components/DomainChangeAlert.tsx
@@ -1,20 +1,18 @@
-import { onMount } from 'solid-js'
 import Cookies from 'js-cookie'
 import './DomainChangeAlert.sass'
 
 const DomainChangeAlert = () => {
-  let dialogElement!: HTMLDialogElement
-
-  onMount(() => {
+  const autoShowModal = (dialogElement: HTMLDialogElement) => {
     const noHideCookie = !Cookies.get('hideDCA')
     const originNotMatch = window.location.origin !== 'https://soci.onebreath.it'
-    if (noHideCookie && originNotMatch) dialogElement.showModal()
-  })
+    // queueMicrotask to wait until the component is mounted
+    if (noHideCookie && originNotMatch) queueMicrotask(() => dialogElement.showModal())
+  }
 
   const handleClose = () => Cookies.set('hideDCA', 'yes', { expires: 1 })
 
   return (
-    <dialog ref={dialogElement} class='domainchange' closedby='any' onClose={handleClose}>
+    <dialog ref={autoShowModal} class='domainchange' closedby='any' onClose={handleClose}>
       <p>
         Ci siamo trasferiti su&nbsp;
         <a href='https://soci.onebreath.it' target='_blank'>https://soci.onebreath.it</a>
@@ -22,7 +20,7 @@ const DomainChangeAlert = () => {
         <b>Dovrai riattivare le notifiche!</b>
       </p>
       <form method='dialog'>
-        <button >Nascondi temporaneamente avviso</button>
+        <button>Nascondi temporaneamente avviso</button>
       </form>
     </dialog>
   )
