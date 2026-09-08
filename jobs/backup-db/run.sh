a534b21d4ed801b2890bc2554d842e78754245ae
Alessio Margelli <alex9446@gmail.com>
2026-09-08 16:35:48 +0200

    encrypt backup with gpg public key

--- jobs/backup-db/run.sh
+++ jobs/backup-db/run.sh
@@ -5,17 +5,19 @@ set -e
 [ -z "$HEARTBEAT_URL" ] && echo "heartbeat url not found!"
 
 echo "Job \"backup-db\" started!"
-curl -m 10 -s -o /dev/null "$HEARTBEAT_URL/start"
+[ -n "$HEARTBEAT_URL" ] && curl -m 10 -s -o /dev/null "$HEARTBEAT_URL/start"
 
 DT=$(date -uIs)
-FILENAME=dump_$DT.sql
-FILEPATH=./$FILENAME
+FILEPATH=./dump_$DT.sql
+FILEPATH_GPG=$FILEPATH.gpg
 
 pg_dump -f $FILEPATH $READ_ALL_CONNECTION_URI
 
-rclone copy $FILEPATH scw-fr-par:supabase-backups/
+gpg --encrypt --recipient-file ./OB-pub-key.asc --output $FILEPATH_GPG $FILEPATH
 
-echo "Dump filename: $FILENAME"
+rclone copy $FILEPATH_GPG scw-fr-par:supabase-backups/
 
-curl -m 10 -s -o /dev/null "$HEARTBEAT_URL"
+echo "Dump file: $FILEPATH_GPG"
+
+[ -n "$HEARTBEAT_URL" ] && curl -m 10 -s -o /dev/null "$HEARTBEAT_URL"
 echo "Job \"backup-db\" finished!"
